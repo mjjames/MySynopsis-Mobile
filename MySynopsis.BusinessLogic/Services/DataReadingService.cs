1838df91eb0fbe2e5a5dec101d65e254b28f555b
Michael James <mike@mjjames.co.uk>
2013-12-28 23:05:16 +0000

    initial implementation of data reading service

---
+++ MySynopsis.BusinessLogic/Services/DataReadingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySynopsis.BusinessLogic.Services
+{
+    public class DataReadingService : IDataReadingService
+    {
+
+        private IMobileServiceClient _serviceClient;
+        public DataReadingService(IMobileServiceClient serviceClient)
+        {
+            _serviceClient = serviceClient;
+        }
+
+        public async Task PersistReadings(IEnumerable<Models.DataReading> readings)
+        {
+            var table = _serviceClient.GetTable<Models.DataReading>();
+            var tasks = new List<Task>();
+            foreach (var reading in readings)
+            {
+                tasks.Add(table.InsertAsync(reading));
+            }
+            await Task.WhenAll(tasks);
+        }
+    }
+}
